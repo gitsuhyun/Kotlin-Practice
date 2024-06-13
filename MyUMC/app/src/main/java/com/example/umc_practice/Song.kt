a35f37ffd48d3821295f1ec3f17a4bf071db21d1
gitsuhyun <suhyun10201@naver.com>
2024-06-14 00:18:33 +0900

    [Feat] umc 복습용 프로젝트 추가

---
+++ MyUMC/app/src/main/java/com/example/umc_practice/Song.kt
@@ -0,0 +1,7 @@
+package com.example.umc_practice
+
+data class Song(
+    val title : String = "",
+    val singer : String = ""
+
+)
---
+++ MyUMC/app/src/main/java/com/example/umc_practice/SongActivity.kt
@@ -0,0 +1,43 @@
+package com.example.umc_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.umc_practice.databinding.ActivityMainBinding
+import com.example.umc_practice.databinding.ActivitySongBinding
+
+class SongActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySongBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //root는 activity_song의 최상단 뷰, 모든 뷰를 맘껏 쓸 수 있음
+        binding.songDownIb.setOnClickListener {
+            finish()
+        }
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener {
+            setPlayerStatus(true)
+        }
+        if(intent.hasExtra("title") && intent.hasExtra("singer")){
+            binding.songMusicTitleTv.text = intent.getStringExtra("title")
+            binding.songSingerNameTv.text = intent.getStringExtra("singer")
+
+        }
+    }
+
+    fun setPlayerStatus(isPlaying: Boolean){
+        if(isPlaying) {
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        } else {
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
+        }
+    }
+}
